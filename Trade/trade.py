8b97a5ac992cdb507388b5172e337760e29cc7e9
100676964 <zhou895230434@hotmail.com>
2021-07-15 23:36:45 -0400

    commit

--- Trade/trade.py
+++ Trade/trade.py
@@ -3,13 +3,20 @@ from statistics import mean,stdev
 # def get_closing_prices(rates:np.array):
 
 def get_action(rates:np.array, avaliable_funs, current_pl):
-    x = 20
-    y = 1.5
-    z = 1.5
+    
+
+    
+    pass
+def __calculate_score(rates,current_pl,number_of_days,H_multiplier,L_multiplier):
+    x = number_of_days
+    y = H_multiplier
+    z = L_multiplier
+    
+    
     current_rate = rates[-1]
     moving_average = mean(rates[-x:])
     H_band = stdev(rates[-x:])*y + moving_average
     L_band = stdev(rates[-x:])*z + moving_average
-
+    
     history_average = mean(rates)
     pass
